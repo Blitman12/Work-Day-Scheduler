faf17b41315c8d08a97443857bfc3ea95cc91860
B-Litty <bradlitman94@gmail.com>
2021-10-08 09:52:38 -0700

    color change based on time completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,31 @@
 $(document).ready(function () {
+    // if tasks exists in localStorage it will be placed here, otherwise it will be an empty array
     let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-
+    let currentTime = moment().format('LT').slice(0, -6)
+    
+    //Change color based off of the current time
     var changeColorOnTime = function () {
-
+        $(".container").find("h3").each(function(index) {
+            let timeBlock = $(this).text().slice(0, -2)
+            console.log(timeBlock)
+            console.log("the current time is: " + currentTime)
+            // if its inbetween the hour block make red
+            if (currentTime === timeBlock) {
+                $(this).siblings("textarea").addClass("present").removeClass("bg-secondary")
+            } 
+            // if its above hour block make green
+            else if (currentTime > timeBlock) {
+                $(this).siblings("textarea").addClass("future").removeClass("bg-secondary")
+            } 
+            //if its below hour block make grey
+            else {
+                $(this).siblings("textarea").addClass("past").removeClass("bg-secondary")
+            }
+        })
     }
 
+    //call the function to change classes based off of time
+    changeColorOnTime()
 
     let loadTasks = function () {
         let loadedTasks = JSON.parse(localStorage.getItem("tasks"))
@@ -21,9 +42,7 @@ $(document).ready(function () {
     // Aquires the users input on a specific time and uploads it to localStorage
     $(".container").on("click", ".saveBtn", function () {
         let loadedTasks = localStorage.getItem("tasks")
-        console.log(loadedTasks + " is the not parsed yet")
         let formatedTasks = JSON.parse(loadedTasks)
-        console.log(formatedTasks + " is the formated")
         if (!formatedTasks) {
             // gets the value the user input into the textarea
             let taskInput = $($(this).siblings("textarea")).val()
