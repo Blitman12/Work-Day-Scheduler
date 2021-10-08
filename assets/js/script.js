eb02ec4aba28307cfa2fb71395b0bbc8ce1643b1
B-Litty <bradlitman94@gmail.com>
2021-10-08 12:24:21 -0700

    time colors created and localStorage set up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,23 @@
 $(document).ready(function () {
     // if tasks exists in localStorage it will be placed here, otherwise it will be an empty array
     let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    let currentTime = moment().format('LT').slice(0, -6)
-    
+    let currentTime = moment().format("HH")
+
     //Change color based off of the current time
     var changeColorOnTime = function () {
-        $(".container").find("h3").each(function(index) {
-            let timeBlock = $(this).text().slice(0, -2)
-            console.log(timeBlock)
-            console.log("the current time is: " + currentTime)
-            // if its inbetween the hour block make red
-            if (currentTime === timeBlock) {
-                $(this).siblings("textarea").addClass("present").removeClass("bg-secondary")
-            } 
+        $(".container").find("h3").each(function () {
+            let timeBlock = $(this).data("time")
+            // if its inbetween the hour block make red. Using double equals because of the string to number
+            if (currentTime == timeBlock) {
+                $(this).siblings("textarea").removeClass("bg-secondary").addClass("present")
+            }
             // if its above hour block make green
-            else if (currentTime > timeBlock) {
-                $(this).siblings("textarea").addClass("future").removeClass("bg-secondary")
-            } 
+            if (timeBlock > currentTime) {
+                $(this).siblings("textarea").removeClass("bg-secondary").addClass("future")
+            }
             //if its below hour block make grey
-            else {
-                $(this).siblings("textarea").addClass("past").removeClass("bg-secondary")
+            if (currentTime < timeBlock) {
+                $(this).siblings("textarea").removeClass("bg-secondary").addClass("past")
             }
         })
     }
@@ -27,12 +25,34 @@ $(document).ready(function () {
     //call the function to change classes based off of time
     changeColorOnTime()
 
+    //call the changeColor function once every 30 minutes
+    setInterval(function () {
+        changeColorOnTime()
+        console.log("I just did it")
+    }, (1000 * 60) * 30)
+
+    // load tasks from local storage then placing them in their right spaces
     let loadTasks = function () {
         let loadedTasks = JSON.parse(localStorage.getItem("tasks"))
         console.log(loadedTasks)
+        // if there is nothing in localStorage then get out of this function
+        if (!loadedTasks) {
+            return
+        }
+        //if there is something in localStorage then place it in its respective spot
+        $(".container").find("h3").each(function () {
+            let textToReplace = $(this).siblings("textarea")
+            let timeBlock = $(this).data("time")
+            for (let i = 0; i < loadedTasks.length; i++) {
+                console.log(timeBlock)
+                if (timeBlock == loadedTasks[i].time) {
+                    textToReplace.val(loadedTasks[i].text)
+                }
+            }
+        })
     }
 
-
+    // Save tasks to localStorage and then loading those tasks
     let saveTasks = function () {
         localStorage.setItem("tasks", JSON.stringify(tasks))
         loadTasks();
@@ -48,29 +68,23 @@ $(document).ready(function () {
             let taskInput = $($(this).siblings("textarea")).val()
             // gets the value of the time slot and removes the am/pm from it to leave a normal number
             let taskTime = $($(this).siblings("textarea").prev()).text().slice(0, -2)
-
             tasks = [{
                 text: taskInput,
                 time: taskTime
             }]
-
             saveTasks();
         } else {
             // gets the value the user input into the textarea
             let taskInput = $($(this).siblings("textarea")).val()
             // gets the value of the time slot and removes the am/pm from it to leave a normal number
-            let taskTime = $($(this).siblings("textarea").prev()).text().slice(0, -2)
+            let taskTime = $($(this).siblings("textarea").prev()).data("time")
             let tempObject = {
                 text: taskInput,
                 time: taskTime
             }
-
             tasks.push(tempObject)
-
             saveTasks();
         }
     })
-
     loadTasks();
-
 })
